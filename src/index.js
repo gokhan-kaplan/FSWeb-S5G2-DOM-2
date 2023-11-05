914c7f88be0519af50afc977cf781ba2698fc12d
gokhan-kaplan <gokhan.kaplan519@outlook.com>
2023-11-05 08:15:22 +0300

    S5G2

--- src/index.js
+++ src/index.js
@@ -1,9 +1,49 @@
-import './less/index.less'
+import "./less/index.less";
 
 // Örnek bir event kullanımı aşağıdadır. Çalıştırmak için comment dışına alın
-// document.querySelector("h1").addEventListener("click",function(e){
-// alert("Bana tıkladın!")
-// });
-
+document.querySelector("h1").addEventListener("click", function (e) {
+  alert("Bana tıkladın!");
+});
 
 // Kodlar buraya gelecek!
+
+document.querySelector("h1").addEventListener("mouseover", function () {
+  this.style.color = "red";
+});
+
+document.querySelector("h1").addEventListener("mouseout", function () {
+  this.style.color = "black";
+});
+
+document.addEventListener("keydown", function (event) {
+  alert("Tuşa basıldı: " + event.key);
+});
+
+window.addEventListener("load", function () {
+  alert("Sayfa yüklendi!");
+});
+
+document.getElementById("firstParag").addEventListener("dblclick", function () {
+  this.style.color = "purple";
+});
+
+document
+  .getElementById("navContact")
+  .addEventListener("click", function (event) {
+    event.preventDefault();
+    alert("İstenen davranış engellendi.");
+  });
+
+document.getElementById("btn").addEventListener("click", displayDate);
+
+document.getElementById("textarea").addEventListener("select", function () {
+  alert("Metin seçildi.");
+});
+
+window.addEventListener("scroll", function () {
+  console.log("Sayfa kaydırıldı.");
+});
+
+document.getElementById("firstParag").addEventListener("select", function () {
+  alert("Metin seçildi.");
+});
